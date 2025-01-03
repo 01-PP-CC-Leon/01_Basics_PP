de7543b595ce677409331156c187c0efbe9c3c95
Leon Ahner <leon-ahner@gmx.de>
2025-01-03 09:18:05 +0000

    02 / Deklaration + Wertzuweisung 1

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -5,3 +5,17 @@
               Blockkommentar 
 
 */
+
+/***** Variablen 01 ********/
+
+// console.log("Hi!");
+// console.log(Hi);
+
+/* Deklaration + Wertzuweisung I */
+
+let firstName; // Deklaration (Definition)
+console.log(firstName); // Ausgabe
+firstName = "Leon I." // Wertzuweisung (Assignment)
+console.log(firstName); // Ausgabe
+let familyName = "Kaiser von Deutschland"; //Deklaration + Wertzuweisung
+console.log("Hi, ich bin " + firstName + " " +familyName + "!" );
