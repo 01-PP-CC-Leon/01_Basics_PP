15205fd2a556642192991cb604df5a92c6290eb5
Leon Ahner <leon-ahner@gmx.de>
2025-01-03 10:09:46 +0000

    02/ Variable vs Konstante

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -13,9 +13,46 @@
 
 /* Deklaration + Wertzuweisung I */
 
-let firstName; // Deklaration (Definition)
-console.log(firstName); // Ausgabe
-firstName = "Leon I." // Wertzuweisung (Assignment)
-console.log(firstName); // Ausgabe
-let familyName = "Kaiser von Deutschland"; //Deklaration + Wertzuweisung
-console.log("Hi, ich bin " + firstName + " " +familyName + "!" );
+// let firstName; // Deklaration (Definition)
+// console.log(firstName); // Ausgabe
+// firstName = "Leon I." // Wertzuweisung (Assignment)
+// console.log(firstName); // Ausgabe
+// let familyName = "Kaiser von Deutschland"; //Deklaration + Wertzuweisung
+// console.log("Hi, ich bin " + firstName + " " +familyName + "!" );
+
+
+/*
+  Wertzuweisung II + Datentypen
+  JS ist eine untypisierte Sprache!
+*/  
+
+// let test;  // Deklaration
+// console.log("Datentyp: " + typeof test );
+// console.log ("Inhalt: " + test);
+// console.log("----------------")
+// test = "Ich bin der Kaiser von Deutschland"; // string
+// console.log("Datentyp: " + typeof test );
+// console.log ("Inhalt: " + test);
+// console.log("----------------")
+// test = 2 // number
+// console.log("Datentyp: " + typeof test );
+// console.log ("Inhalt: " + test);
+// console.log("----------------")
+// test = true; // boolean
+// console.log("Datentyp: " + typeof test );
+// console.log ("Inhalt: " + test);
+// console.log("----------------")
+
+/* Variablen vs. Konstanten in JS */
+
+// Variable
+
+let test;  // Variable: Deklaration
+test = "hi"; // Wertzuweisung (Initialisierung)
+test = "hello"; // Überschreiben
+console.log("Inhalt: " + test); // Ausgabe
+
+// Konstante
+const test2 = "Hi!"; // Deklaration + Wertzuweisung gemeinsam !
+test2 = " Wo ist Oktay?"; //Laufzeitfehler !!
+console.log("Inhalt: " + test2); // Ausgabe
