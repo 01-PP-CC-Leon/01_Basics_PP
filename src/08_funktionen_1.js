7bbccb465c49d7569876bdb4baffa26264525d0d
Leon Ahner <leon-ahner@gmx.de>
2025-01-08 09:16:38 +0000

    08 / // 2b. Parametrisierung + Datenübergabe von AUSSEN

--- src/08_funktionen_1.js
+++ src/08_funktionen_1.js
@@ -29,3 +29,13 @@ function ausgabeNamen() {
 
 // console.log(firstName); // Fehler :  scope!
 
+/***** Funktionen 02b *****/
+// 2b. Parametrisierung + Datenübergabe von AUSSEN
+
+ausgabeNamen2("Leon"); // Argument --> Daten für Parameter
+ausgabeNamen2("Max");
+
+
+function ausgabeNamen2(firstName) { // Parameter
+       console.log("Hallo, " + firstName + "!");
+}
