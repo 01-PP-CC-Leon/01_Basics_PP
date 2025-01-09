626a1e385e16c2d60a637f2cef4355252fa072d6
Leon Ahner <leon-ahner@gmx.de>
2025-01-09 09:18:53 +0000

    09 / division 1.Iteration

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -9,6 +9,19 @@
 */
 
 
+// module: division a / b |  test:
+output(divide(4,2));
+output(divide(3,2));
+output(divide(3,-2));
+output(divide(0,2));
+output(divide(3,0));
+output(divide(0,0));
+function divide(a,b) {
+    return a / b
+}
+
+
+
 // module: multiplication a * b |  test:
 // output(multiply(3,2));
 // output(multiply(3,-2));
