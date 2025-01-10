52b4b97c16483b3c7ccaa228467451cb5028c69a
Leon Ahner <leon-ahner@gmx.de>
2025-01-10 09:51:27 +0000

    11 / 02. Theorie: Array

--- src/11_Arrays_I.js
+++ src/11_Arrays_I.js
@@ -7,7 +7,16 @@
 
 /*** 02. Theorie: Array */
 
+let arr;
+// arr = new array ();   // Konstruktor 
+arr = [];  // Literal
+arr = [2,11,7,8,10,69];
 
+
+output (arr);    
+output (arr.length); // Anzahl der Elemente
+output(arr[0]); // Index 0 (1.Pos. im Array)
+output(arr[arr.length-1]); // letzte Position / allgemein
 
 
 /**
@@ -19,7 +28,7 @@
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
 // output(getSentence("Ich","bin","Max","Mütze"));
-output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
+// output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
 function getSentence(word1,word2,word3,word4,word5,word6) {
     const GAP = " ";
     const PUNCT = ".";
