2fc324dba15c903ec66f2272bc7b01aed6bef63e
Leon Ahner <leon-ahner@gmx.de>
2025-01-13 08:34:07 +0000

    13 / Arrays Push und Pop

--- src/12_Codechallenge_III.js
+++ src/12_Codechallenge_III.js
@@ -11,39 +11,39 @@
 
 /*** Funktion mit Array als Parameter */
 // mögliche Tests:
-output(getSentence(["Ich","bin","Max"], "S"));
-output(getSentence(["Bist","du","Max"], "Q"));
-output(getSentence(["Ich","bin","Max"], "E"));
+// output(getSentence(["Ich","bin","Max"], "S"));
+// output(getSentence(["Bist","du","Max"], "Q"));
+// output(getSentence(["Ich","bin","Max"], "E"));
 
 
 function getSentence(arr,op) {
-    const GAP = " ";
-    const PUNCT = getPunct(op);  // . ? !
-    let str = ""; 
+    // const GAP = " ";
+    // const PUNCT = getPunct(op);  // . ? !
+    // let str = ""; 
 
-    for (let i = 0; i < arr.length; i++) {
-        if (i != arr.length-1) {
-            str += arr[i] + GAP;
-        } else {
-            str += arr[i] + PUNCT; 
-        }
-    }
-     return str;
+    // for (let i = 0; i < arr.length; i++) {
+    //     if (i != arr.length-1) {
+    //         str += arr[i] + GAP;
+    //     } else {
+    //         str += arr[i] + PUNCT; 
+    //     }
+    // }
+    //  return str;
 }
 
 
 function getPunct(op) {
-    switch (op) {
-        case "S":
-           return "."; 
-        case "Q":
-            return "?";
-        case "E":
-            return "!";
-        default:
-            return "#";
+    // switch (op) {
+    //     case "S":
+    //        return "."; 
+    //     case "Q":
+    //         return "?";
+    //     case "E":
+    //         return "!";
+    //     default:
+    //         return "#";
     }
-}
+
 
 
 // Modul: Ausgabe in Konsole : Test
